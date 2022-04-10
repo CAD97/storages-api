b18e7db93c366c75908a4587470904968ff42b0e
CAD97 <cad97@cad97.com>
2022-04-10 22:16:39 +0000

    Deploying to gh-pages from @ CAD97/storages-api@c24b922cb58bbf348e1bc504257ccf857c6fdc67 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["storage_api"] = {"name":"","files":["alloc.rs","inline.rs","lib.rs","polyfill.rs","raw_box.rs","raw_vec.rs","small.rs","traits.rs"]};
+sourcesIndex["storage_api"] = {"name":"","files":["alloc.rs","inline.rs","lib.rs","polyfill.rs","raw_box.rs","raw_vec.rs","referenced.rs","small.rs","traits.rs"]};
 createSourceSidebar();
