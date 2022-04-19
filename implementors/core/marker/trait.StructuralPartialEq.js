757e3a5441391f3d75b3965a09c50a4c49e61b33
CAD97 <cad97@cad97.com>
2022-04-19 08:15:35 +0000

    Deploying to gh-pages from @ CAD97/storages-api@c7194d733dd46e38c5085d67c0a441aca5106bc2 🚀

---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["storage_api"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"storage_api/struct.AllocHandle.html\" title=\"struct storage_api::AllocHandle\">AllocHandle</a>","synthetic":false,"types":["storage_api::alloc::AllocHandle"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
